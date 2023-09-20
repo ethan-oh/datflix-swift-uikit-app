88f0e31426e0bec79adb7f517d26338a97d7ac2e
Minsung Kim <ms990310@gmail.com>
2023-09-20 15:37:33 +0900

    initial project

---
+++ season4_mainproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  season4_mainproject
+//
+//  Created by 김민성 on 2023/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
